efa37e3cf972e97bb025a501b81aa406a243ae9c
AquaPirot <acaglobus@gmail.com>
2026-02-21 18:04:39 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Service Worker — Podsetnik PROHORECA
 // Verzija cache-a — promeni kad update-uješ app
-const CACHE_NAME = 'podsetnik-v2';
+const CACHE_NAME = 'podsetnik-v3';
 
 const ASSETS = [
   './',
